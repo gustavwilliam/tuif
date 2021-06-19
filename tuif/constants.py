01b5f278b2f932abcf2842a24326a4148e170f6d
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-06-19 02:23:01 +0200

    Update constants loader to create color objects
    constants.colors will now have color objects, instead of strings. That's
    how colors are represented, so this implementation is more direct and
    easy to use in the rest of the code

--- tuif/constants.py
+++ tuif/constants.py
@@ -1,15 +1,22 @@
-from dataclasses import dataclass
+from tuif.models.color import Color
 
 import yaml
 
 
-@dataclass
 class Colors:
-    white: int
-    black: int
-    red: int
-    blue: int
-    green: int
+    def __init__(
+        self,
+        white: int,
+        black: int,
+        red: int,
+        blue: int,
+        green: int,
+    ) -> None:
+        self.white = Color(hex(white))
+        self.black = Color(hex(black))
+        self.red = Color(hex(red))
+        self.blue = Color(hex(blue))
+        self.green = Color(hex(green))
 
 
 with open("constants.yaml", "r") as f:
