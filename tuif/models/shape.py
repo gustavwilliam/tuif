2b48c06d800bc0b46bcdd283b75e0dbb6994c1fd
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-06-19 23:38:58 +0200

    Make border and fill optional for all shapes

--- tuif/models/shape.py
+++ tuif/models/shape.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import List, Optional
 
+from tuif.constants import colors
 from tuif.models.border import Border
 from tuif.models.color import Color
 from tuif.models.constraints import Constraint, Offset, RelativePosition
@@ -9,13 +10,16 @@ from tuif.models.constraints import Constraint, Offset, RelativePosition
 class Shape:
     """Base class for all shapes.
 
-    :param fill: Fill-color of the shape
-    :type fill: :class:`tuif.models.color.Color`
-    :param border: Border of the shape
-    :type border: :class:`tuif.models.border.Border`
+    :param fill: Fill-color of the shape, defaults to :class:`tuif.constants.colors.white`
+    :type fill: :class:`tuif.models.color.Color`, optional
+    :param border: Border of the shape, default to a border with width 0
+    :type border: :class:`tuif.models.border.Border`, optional
     """
 
-    def __init__(self, fill: Color, border: Border) -> None:
+    def __init__(
+        self, fill: Color = Color(colors.white), border: Border = Border(width=0)
+    ) -> None:
+        """Constructor method."""
         self.fill = fill
         self.border = border
 
@@ -43,10 +47,14 @@ class Rectangle(Shape):
         self,
         width: int,
         height: int,
-        fill: Optional[Color],
-        border: Optional[Border],
+        fill: Optional[Color] = None,
+        border: Optional[Border] = None,
     ) -> None:
         """Constructor method."""
-        super().__init__(fill, border)
+        if fill and border:
+            super().__init__(fill, border)
+        else:
+            super().__init__()
+
         self.width = width
         self.height = height
