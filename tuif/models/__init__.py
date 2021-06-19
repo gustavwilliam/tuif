29f551b0819c04b15293e628b321a9fe6a3f4256
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-06-19 21:26:21 +0200

    Update name of frame class to canvas
    Canvas better reflects what the class is intended to do. It stores the
    state of the screen as a canvas — not as a frame.

--- tuif/models/__init__.py
+++ tuif/models/__init__.py
@@ -2,4 +2,4 @@ from tuif.models.color import Color
 from tuif.models.border import Border
 
 from tuif.models.shape import Shape, Rectangle
-from tuif.models.frame import Frame
+from tuif.models.canvas import Canvas
--- tuif/models/canvas.py
+++ tuif/models/canvas.py
@@ -3,7 +3,7 @@ from tuif.models.color import Color
 from tuif.models.shape import Shape
 
 
-class Frame:
+class Canvas:
     def __init__(
         self, *objects: Tuple[Shape], background_color: Optional[Color] = None
     ) -> None:
@@ -11,19 +11,19 @@ class Frame:
         self.objects: List[Shape] = list(objects)
 
     def add(self, object_: Shape) -> None:
-        """Adds the provided object to the top of the frame.
+        """Adds the provided object to the top of the canvas.
 
-        :param object_: Object/shape to add to the frame
+        :param object_: Object/shape to add to the canvas
         :type object_: :class:`tuif.models.shape.Shape`
         """
         self.objects.append(object_)
 
     def insert(self, object_: Shape, index: int) -> None:
-        """Adds the provided object to the top of the frame.
+        """Adds the provided object to the top of the canvas.
 
-        :param object_: Object/shape to add to the frame
+        :param object_: Object/shape to add to the canvas
         :type object_: :class:`tuif.models.shape.Shape`
-        :param index: Index for where in the frame the object should be inserted. 0 is the very top of the frame
+        :param index: Index for where in the canvas the object should be inserted. 0 is the very top of the canvas
         :type index: int
         """
         self.objects.insert(index, object_)
