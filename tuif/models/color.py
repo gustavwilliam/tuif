2f387a45b75ad94383162e71612f5006399f09f6
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-06-20 15:25:57 +0200

    Update color model to use colorama
    Terminals can often only use ANSI colors, so colorama will ensure that
    those are the only colors used by TUIF

--- tuif/models/color.py
+++ tuif/models/color.py
@@ -1,20 +1,37 @@
-from dataclasses import dataclass
+import colorama
 
 
-@dataclass
 class Color:
     """Class for managing colors.
 
-    :param hex: Hexadecimal color code
-    :type hex: int
+    :param name: ANSI name of the color. Only accepts valid ANSI color names that are supported by colorama
+    :type name: str
+    :param type_: The type of color (foreground or background). Accepts "Fore" for foreground and "Back" for background, defaults to "Back"
+    :type type_: str
     """
 
-    hex: int
+    def __init__(self, name: str, type_: str = "Back") -> None:
+        """Constructor method."""
+        if type_ not in ["Fore", "Back"]:
+            raise ValueError(
+                f"'{type_}' is not a valid color type. Accepts 'Fore' and 'Back' as arguments."
+            )
+        self.type_ = type_
+        self.colorama_type: colorama.AnsiCodes = getattr(colorama, self.type_)
+
+        ANSI_NAMES = list(vars(self.colorama_type).keys())
+        name = name.upper()  # Colorama color names are all uppercase.
+        if name not in ANSI_NAMES:
+            raise ValueError(
+                f"'{name}' is not a valid ANSI color name. Please select one of the following: {ANSI_NAMES}"
+            )
+        self.name = name
+        self.ansi = getattr(self.colorama_type, self.name)
 
     def __str__(self) -> str:
-        """Returns hexadecimal string representation of the color.
+        """Returns a lowercase string representation of the color.
 
-        :return: Hexadecimal string representation of the color, such as "#FFD45A".
+        :return: String representation of the color, such as "white" or "red".
         :rtype: str
         """
-        return f"#{self.hex[2:]}"
+        return self.name.lower()
