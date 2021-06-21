abe17c76273382218d2b85a861afb36a6c1eae8c
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-06-21 13:35:48 +0200

    Update Border class to use new Color class

--- tuif/models/border.py
+++ tuif/models/border.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 
-from tuif.constants import colors
 from tuif.models.color import Color
 
 
@@ -10,12 +9,12 @@ class Border:
 
     :param width: Width (in pixels), defaults to 1
     :type width: int, optional
-    :param color: Color of the border, defaults to :class:`tuif.constaints.colors.black`
+    :param color: Color of the border, defaults to black
     :type color: :class:`tuif.models.color.Color`, optional
     :param border_radius: Border radius (in pixels), defaults to 0
     :type border_radius: int, optional
     """
 
     width: int = 1
-    color: Color = colors.black
+    color: Color = Color("black")
     border_radius: int = 0
